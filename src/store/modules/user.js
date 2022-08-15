b2d5d62989d94c2014b9c424ca19e707d2e9a555
cv-工程师 <10488835+liu-fangfeng@user.noreply.gitee.com>
2022-08-15 15:59:53 +0800

    持久化完成

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,19 +1,27 @@
 import { login } from '@/api/user'
-import { setToken } from '@/utils/auth'
+import { setToken, getToken, removeToken } from '@/utils/auth' // 引入封装好的本地存储
 export default {
   namespaced: true,
+
   state: {
-    // token默认是空
-    token: null
+    // token持久化从本地拿token
+    token: getToken()
   },
+
   mutations: {
     // 修改token
     SETTOKEN(state, token) {
       state.token = token
       // 把token存到本地存储
       setToken(token)
+    },
+    // 清空token
+    removeToken(state) {
+      state.token = null
+      removeToken()
     }
   },
+
   actions: {
     // 登录获取token
     async login({ commit }, data) {
