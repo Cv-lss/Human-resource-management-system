e5592923863e77fe63a3a0e4ddc1ca1eb2e9362a
cv-工程师 <10488835+liu-fangfeng@user.noreply.gitee.com>
2022-08-19 09:11:46 +0800

    路由前置守卫

---
+++ src/permission.js
@@ -0,0 +1,26 @@
+
+import router from '@/router'
+import store from '@/store'
+const whiteList = ['/login', '/404'] // 定义白名单  所有不受权限控制的页面
+
+// 路由前置守卫
+router.beforeEach((to, from, next) => {
+  // 判断里面有没有token
+  if (store.getters.token) {
+    // 如果有token 但是还想去登录界面
+    if (to.path === '/login') {
+      // 就不跳转 直接在主页面
+      next('/')
+    } else {
+      // 如果没有 就放行
+      next()
+    }
+  } else { // 如果没有token
+    // 然后白名单是可以跳转的 login 和404可以跳转
+    if (whiteList.includes(to.path)) {
+      next()
+    } else { // 如果不是去白名单 就到login页
+      next('/login') // 跳到登录页
+    }
+  }
+})
